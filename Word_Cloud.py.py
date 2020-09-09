c018d28c4f5b99cbdfe6e0a9b16dab9e3a991c1d
raksalim <raksalim@users.noreply.github.com>
2020-09-09 17:19:18 -0400

    Imported all modules
    All modules required have been imported

--- Word_Cloud.py.py
+++ Word_Cloud.py.py
@@ -1,13 +1,17 @@
-file_contents = open('Little_red.txt', 'r').read()
+import matplotlib as mp
+import wordcloud as wc
 
-def calculate_frequencies(file_contents):
+
+file_contents = open('Michelle_Obama_Thoughts_On_Trump.txt', 'r').read()
+
+might_del = """def calculate_frequencies(file_contents):
     # Here is a list of punctuations and uninteresting words you can use to process your text
     punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
     uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
     "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them", \
     "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being", \
     "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
-    "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
+    "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]"""
     
 # LEARNER CODE START HERE
 
